a5c610bf076121f8b41f545d20ac16683d468af2
Marlena Kurczab <mojca13@gazeta.pl>
2022-05-28 22:41:09 +0200

    koniec.

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace ViveCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNrKoszulki = new System.Windows.Forms.TextBox();
+            this.txtImie = new System.Windows.Forms.TextBox();
+            this.txtNazwisko = new System.Windows.Forms.TextBox();
+            this.txtDataUrodzenia = new System.Windows.Forms.TextBox();
+            this.txtPozycja = new System.Windows.Forms.TextBox();
+            this.txtKraj = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnZapisz = new System.Windows.Forms.Button();
+            this.btnUsun = new System.Windows.Forms.Button();
+            this.btnAnuluj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nr koszulki";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "imię";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "nazwisko";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "data urodzenia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "pozycja";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "kraj";
+            // 
+            // txtNrKoszulki
+            // 
+            this.txtNrKoszulki.Location = new System.Drawing.Point(142, 40);
+            this.txtNrKoszulki.Name = "txtNrKoszulki";
+            this.txtNrKoszulki.Size = new System.Drawing.Size(100, 22);
+            this.txtNrKoszulki.TabIndex = 6;
+            // 
+            // txtImie
+            // 
+            this.txtImie.Location = new System.Drawing.Point(142, 65);
+            this.txtImie.Name = "txtImie";
+            this.txtImie.Size = new System.Drawing.Size(100, 22);
+            this.txtImie.TabIndex = 7;
+            // 
+            // txtNazwisko
+            // 
+            this.txtNazwisko.Location = new System.Drawing.Point(142, 93);
+            this.txtNazwisko.Name = "txtNazwisko";
+            this.txtNazwisko.Size = new System.Drawing.Size(100, 22);
+            this.txtNazwisko.TabIndex = 8;
+            // 
+            // txtDataUrodzenia
+            // 
+            this.txtDataUrodzenia.Location = new System.Drawing.Point(142, 121);
+            this.txtDataUrodzenia.Name = "txtDataUrodzenia";
+            this.txtDataUrodzenia.Size = new System.Drawing.Size(100, 22);
+            this.txtDataUrodzenia.TabIndex = 9;
+            // 
+            // txtPozycja
+            // 
+            this.txtPozycja.Location = new System.Drawing.Point(142, 149);
+            this.txtPozycja.Name = "txtPozycja";
+            this.txtPozycja.Size = new System.Drawing.Size(100, 22);
+            this.txtPozycja.TabIndex = 10;
+            // 
+            // txtKraj
+            // 
+            this.txtKraj.Location = new System.Drawing.Point(142, 177);
+            this.txtKraj.Name = "txtKraj";
+            this.txtKraj.Size = new System.Drawing.Size(100, 22);
+            this.txtKraj.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(309, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(929, 355);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // btnZapisz
+            // 
+            this.btnZapisz.Location = new System.Drawing.Point(22, 260);
+            this.btnZapisz.Name = "btnZapisz";
+            this.btnZapisz.Size = new System.Drawing.Size(75, 23);
+            this.btnZapisz.TabIndex = 13;
+            this.btnZapisz.Text = "zapisz";
+            this.btnZapisz.UseVisualStyleBackColor = true;
+            this.btnZapisz.Click += new System.EventHandler(this.btnZapisz_Click);
+            // 
+            // btnUsun
+            // 
+            this.btnUsun.Location = new System.Drawing.Point(129, 260);
+            this.btnUsun.Name = "btnUsun";
+            this.btnUsun.Size = new System.Drawing.Size(75, 23);
+            this.btnUsun.TabIndex = 14;
+            this.btnUsun.Text = "usuń";
+            this.btnUsun.UseVisualStyleBackColor = true;
+            this.btnUsun.Click += new System.EventHandler(this.btnUsun_Click);
+            // 
+            // btnAnuluj
+            // 
+            this.btnAnuluj.Location = new System.Drawing.Point(228, 260);
+            this.btnAnuluj.Name = "btnAnuluj";
+            this.btnAnuluj.Size = new System.Drawing.Size(75, 23);
+            this.btnAnuluj.TabIndex = 15;
+            this.btnAnuluj.Text = "anuluj";
+            this.btnAnuluj.UseVisualStyleBackColor = true;
+            this.btnAnuluj.Click += new System.EventHandler(this.btnAnuluj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1259, 450);
+            this.Controls.Add(this.btnAnuluj);
+            this.Controls.Add(this.btnUsun);
+            this.Controls.Add(this.btnZapisz);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtKraj);
+            this.Controls.Add(this.txtPozycja);
+            this.Controls.Add(this.txtDataUrodzenia);
+            this.Controls.Add(this.txtNazwisko);
+            this.Controls.Add(this.txtImie);
+            this.Controls.Add(this.txtNrKoszulki);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Łomża Vive Kielce-złota ekipa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNrKoszulki;
+        private System.Windows.Forms.TextBox txtImie;
+        private System.Windows.Forms.TextBox txtNazwisko;
+        private System.Windows.Forms.TextBox txtDataUrodzenia;
+        private System.Windows.Forms.TextBox txtPozycja;
+        private System.Windows.Forms.TextBox txtKraj;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnZapisz;
+        private System.Windows.Forms.Button btnUsun;
+        private System.Windows.Forms.Button btnAnuluj;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViveCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        klubEntities db = new klubEntities();
+        Druzyna gracz = new Druzyna();
+        int PlayerId = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClearData();
+            SetDataInGridView();
+        }
+        public void ClearData()
+        {
+            txtNrKoszulki.Text = txtImie.Text = txtNazwisko.Text = txtDataUrodzenia.Text = txtPozycja.Text= txtPozycja.Text = string.Empty;
+            btnUsun.Enabled = false;
+            btnZapisz.Text = "Zapisz";
+            PlayerId = 0;
+        }
+
+        public void SetDataInGridView()
+        {
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = db.Druzyna.ToList();
+        }
+        private void btnZapisz_Click(object sender, EventArgs e)
+        {
+            gracz.NrKoszulki = Convert.ToInt32(txtNrKoszulki.Text.Trim());
+            gracz.Imie = txtImie.Text.Trim();
+            gracz.Nazwisko = txtNazwisko.Text.Trim();
+            gracz.DataUrodzenia = Convert.ToDateTime(txtDataUrodzenia.Text.Trim());
+            gracz.Pozycja = txtPozycja.Text.Trim();
+            gracz.Kraj = txtKraj.Text.Trim();
+            if (PlayerId > 0)
+                db.Entry(gracz).State = EntityState.Modified;
+            else
+            {
+                db.Druzyna.Add(gracz);
+            }
+            db.SaveChanges();
+            ClearData();
+            SetDataInGridView();
+            MessageBox.Show("Dane zawodnika zapisane");
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell.RowIndex != -1)
+            {
+                PlayerId = Convert.ToInt32(dataGridView1.CurrentRow.Cells["NrZawodnika"].Value);
+                gracz = db.Druzyna.Where(x => x.NrZawodnika == PlayerId).FirstOrDefault();
+                txtNrKoszulki.Text = gracz.NrKoszulki.ToString();
+                txtImie.Text = gracz.Imie;
+                txtNazwisko.Text = gracz.Nazwisko;
+                txtDataUrodzenia.Text = gracz.DataUrodzenia.ToString();
+                txtPozycja.Text = gracz.Pozycja;
+                txtKraj.Text = gracz.Kraj;
+            }
+            btnZapisz.Text = "Odśwież";
+            btnUsun.Enabled = true;
+        }
+
+        private void btnUsun_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Jesteś pewny,że chcesz usunąć zawodnika z listy ?", "Usunąć ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                db.Druzyna.Remove(gracz);
+                db.SaveChanges();
+                ClearData();
+                SetDataInGridView();
+                MessageBox.Show("dane usunięte");
+            }
+        }
+
+        private void btnAnuluj_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+    }
+}
